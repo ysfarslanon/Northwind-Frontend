d11c9716afce88a14a34ce83b726b62ae7938f26
Yusuf ARSLAN <arslanon@outlook.com>
2021-03-17 22:52:55 +0300

    3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import{HttpClientModule} from '@angular/common/http';
 import{FormsModule} from '@angular/forms';
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,8 @@ import { NaviComponent } from './components/navi/navi.component';
 import { from } from 'rxjs';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
+
+import {ToastrModule} from "ngx-toastr";
 
 @NgModule({
   declarations: [//component kullanacağın zaman buraya ekleyeceksin
@@ -26,7 +29,11 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass:"toast-bottom-right"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
